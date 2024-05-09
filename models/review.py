b753750d7840f829dc45e89d8049268e651fabae
Christiana <christine.anako@gmail.com>
2024-05-09 15:50:00 +0100

    update files

--- models/review.py
+++ models/review.py
@@ -1,12 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+""" holds class Review"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
-"""
-Host details of class Review. Extends the BaseModel class
-"""
 
-from base_model import BaseModel
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
 
-class Review(BaseModel):
     def __init__(self, *args, **kwargs):
-        """Initialise the instance for Review"""
+        """initializes Review"""
         super().__init__(*args, **kwargs)
