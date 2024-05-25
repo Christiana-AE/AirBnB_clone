c566a2db2e653b7da7417cb992e1434597bcd746
christiana <christine.anako@gmail.com>
2024-05-25 10:23:21 -0700

    Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Display “Python ” and value of the text variable
-replace underscore _ symbols with a space
-The default value of text is “is cool”
+start Flask application
 """
 
 from flask import Flask
@@ -11,28 +9,27 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """return Hello HBNB"""
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """return HBNB"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display "C" followed by value of test"""
-    return "C " + text.replace("_", " ")
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('python/', strict_slashes=False)
-@app.route('python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the text variable value"""
-    return "Python " + text.replace("_", " ")
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
